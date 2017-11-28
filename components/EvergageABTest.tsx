c52c5e44afb74b59fd8e717546dd41771ff598d1
Patrick Dunn <patwritescode@users.noreply.github.com>
2017-11-28 15:54:34 -0500

    add linter and fix errors

--- components/EvergageABTest.tsx
+++ components/EvergageABTest.tsx
@@ -2,15 +2,15 @@ import * as React from "react";
 import * as PropTypes from "prop-types";
 import * as indexOf from "array-index-of";
 
-const canUseDOM = typeof window != 'undefined';
+const canUseDOM = typeof window !== "undefined";
 
-export interface Variant {
+export interface IVariant {
     name: string;
     node: JSX.Element;
 }
 
 export interface IEvergageABTestProps {
-    variants: Array<Variant>;
+    variants: IVariant[];
     campaign: string;
     eventPrefix: string;
     timeout: number;
@@ -24,66 +24,66 @@ export interface IEvergageABTestState {
 }
 
 export default class EvergageABTest extends React.Component<IEvergageABTestProps, IEvergageABTestState> {
-    public static defaultProps: Partial<IEvergageABTestProps> = {        
+    public static defaultProps: Partial<IEvergageABTestProps> = {
         eventPrefix: "EvergageAB",
         timeout: 100,
-        defaultExperience: 0
+        defaultExperience: 0,
     };
-    constructor(props){
+    constructor (props) {
         super(props);
         this.state = {
             selectedExperience: null,
-            campaignEventReceived: false
-        }
+            campaignEventReceived: false,
+        };
         this.handleEvent = this.handleEvent.bind(this);
         this.checkForExperience = this.checkForExperience.bind(this);
     }
-    componentDidMount(){
-        if(!canUseDOM){
+    public componentDidMount () {
+        if(!canUseDOM) {
             return;
         }
         const { checkForExperience, handleEvent, props : { campaign,  eventPrefix, timeout } } = this;
         window.addEventListener(`${eventPrefix}-${campaign}`, handleEvent);
-        if(document.readyState == 'complete'){            
+        if(document.readyState === "complete") {
             window.setTimeout(checkForExperience, timeout);
             return;
         }
-        window.addEventListener('load', function() {
+        window.addEventListener("load", () => {
             window.setTimeout(checkForExperience, timeout);
         });
     }
-    componentWillUnmount(){
+    public componentWillUnmount () {
         const { checkForExperience, handleEvent, props : { campaign,  eventPrefix, timeout } } = this;
         window.removeEventListener(`${eventPrefix}-${campaign}`, handleEvent);
     }
-    checkForExperience(){
-        if(!this.state.campaignEventReceived && !this.props.supressFallback){            
+    public checkForExperience () {
+        if(!this.state.campaignEventReceived && !this.props.supressFallback) {
             this.setState({
-                selectedExperience: this.props.defaultExperience
-            })
+                selectedExperience: this.props.defaultExperience,
+            });
         }
     }
-    handleEvent(listener){
+    public handleEvent (listener) {
         const {detail : experience} = listener;
         const notFoundIndex = -1;
-        const chosenExperience = indexOf(this.props.variants, experience.variant, function(inArr, variant) {
+        const chosenExperience = indexOf(this.props.variants, experience.variant, (inArr, variant) => {
             return inArr.name === variant;
-        }); 
-        if(chosenExperience != notFoundIndex){
+        });
+        if(chosenExperience !== notFoundIndex) {
             this.setState({
-                selectedExperience: chosenExperience
-            })
+                selectedExperience: chosenExperience,
+            });
         }
         this.setState({
-            campaignEventReceived: true
-        })
+            campaignEventReceived: true,
+        });
     }
-    public render(){
-        if(!canUseDOM){
+    public render () {
+        if(!canUseDOM) {
             return <div />;
         }
         const { variants, campaign } = this.props;
-        if(this.state.selectedExperience == null){
+        if(this.state.selectedExperience == null) {
             return <div />;
         }
         const experience = variants[this.state.selectedExperience];
